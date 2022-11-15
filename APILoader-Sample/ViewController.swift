7f39a9303bf9676f78db1d4a67846da521ce875e
yabuki829 <syodai829@icloud.com>
2022-11-15 16:19:08 +0900

    Initial Commit

---
+++ APILoader-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APILoader-Sample
+//
+//  Created by 薮木翔大 on 2022/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
